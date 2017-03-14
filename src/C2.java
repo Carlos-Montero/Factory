f58aa92fad75687340b2014d5d9626ce78dbfaa5
Carlos <carlosmonteroreina@gmail.com>
2017-03-14 15:52:46 +0100

    Factory da solo un fallo no muy importante.

--- src/C2.java
+++ src/C2.java
@@ -3,6 +3,7 @@
  */
 public class C2 implements Command {
     public void doSomething() {
+
         System.out.println("C2");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,18 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 public class Main {
-    private static final Logger LOGGER = Log.Manager.getLogger(Main.class);
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
     public static void main(String[] args){
 
     try {
-        Factory.getInstance().getCommand("C1").DoSomething();
-        Factory.getInstance().getCommand("C1").DoSomething();
-        Factory.getInstance().getCommand("C1").DoSomething();
-        Factory.getInstance().getCommand("C2").DoSomething();
-        Factory.getInstance().getCommand("C3").DoSomething();
-        Factory.getInstance().getCommand("C3").DoSomething();
-        Factory.getInstance().getCommand("C3").DoSomething();
+        Factory.getInstance().getCommand("C1").doSomething();
+        Factory.getInstance().getCommand("C1").doSomething();
+        Factory.getInstance().getCommand("C1").doSomething();
+        Factory.getInstance().getCommand("C2").doSomething();
+        Factory.getInstance().getCommand("C3").doSomething();
+        Factory.getInstance().getCommand("C3").doSomething();
+        Factory.getInstance().getCommand("C3").doSomething();
 
     }
 
