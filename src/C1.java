6f1bbe63e5991abd553ff5730b8bf7a4a6c327f2
Carlos <carlosmonteroreina@gmail.com>
2017-03-15 20:03:08 +0100

    Factory acabado.

--- src/C1.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Carlos on 08/03/2017.
- */
-public class C1 implements Command {
-    public void doSomething() {
-        System.out.println("C1");
-    }
-}
--- src/Command.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Carlos on 08/03/2017.
- */
-public interface Command {
-    void doSomething();
-
-}
